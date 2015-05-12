6b408e7ba857f6224c2aa3b671993d3b4ce14668
Samuel Jenness <sjenness@uw.edu>
2015-05-11 21:20:20 -0700

    Add R package structure

---
+++ R/concurrency.sim-package.R
@@ -0,0 +1,21 @@
+
+#' concurrency.sim Package
+#'
+#' \tabular{ll}{
+#'    Package: \tab EpiModel.hiv\cr
+#'    Type: \tab Package\cr
+#'    Version: \tab 0.1\cr
+#'    Date: \tab 2015-04-15\cr
+#'    License: \tab GPL (>= 2)\cr
+#'    LazyLoad: \tab yes\cr
+#' }
+#'
+#' @details
+#' EpiModel extensions for modeling HIV infection among heterosexual populations.
+#'
+#' @name concurrency.sim-package
+#' @docType package
+#' @keywords package
+#' @importFrom EpiModel transco
+#'
+NULL
---
+++ R/plot.R
@@ -0,0 +1,50 @@
+
+#' @title Plot Values from a Concurrency Microsimulation Model
+#'
+#' @description Plots values from an concurrency microsimulation
+#'              epidemic model simulated with \code{conc_microsim}.
+#'
+#' @param x an object of class \code{conc_microsim}.
+#' @param xlim x-axis scale limits for plot, with default based on model time steps.
+#' @param ylim y-axis scale limits for plot, with default based on range of
+#'        simulated data.
+#' @param alpha transparency level for simulation lines, where 0 = transparent
+#'        and 1 = opaque (see \code{\link{transco}}).
+#' @param lwd line width for simulation lines.
+#' @param ... additional arguments to pass to main plot window (see
+#'        \code{\link{plot.default}}).
+#'
+#' @details
+#' This function plots the disease prevalence from an \code{conc_microsim}
+#' model. The function is currently limited to individual simulation lines of
+#' disease prevalence. Future releases will standardize the plotting options to
+#' those available with stochastic epidemic models, similar to those in
+#' \code{\link{plot.icm}}.
+#'
+#' @method plot conc_microsim
+#' @keywords internal
+#' @export
+#'
+plot.conc_microsim <- function(x,
+                               xlim,
+                               ylim,
+                               alpha = 1,
+                               lwd = 1,
+                               ...) {
+
+  nsims <- length(x)
+  nsteps <- nrow(x[[1]])
+  pal <- transco(brewer.pal(3, "Set1"), alpha)
+
+  if (missing(ylim)) ylim <- 0:1
+  if (missing(xlim)) xlim <- c(0, nsteps)
+
+  plot(1, 1, type="n", xlim = xlim, ylim = ylim, bty="n",
+       xlab="Time (months)", ylab="Infected", ...)
+  for (i in 1:nsims) {
+    lines(1:nsteps, x[[i]][,1], col = pal[1], lwd = lwd)
+    lines(1:nsteps, x[[i]][,2], col = pal[2], lwd = lwd)
+  }
+  legend("topleft", c("Female", "Male"), lwd=3, col=pal[1:2], bty="n", cex=0.9)
+
+}
